af18375fdc3b38a61763b61e2e23a4396a3ac836
dansmith09 <dansmith_09@hotmail.com>
2022-05-31 22:19:07 +1000

    JS Object change and HTML bug fix

--- logic.js
+++ logic.js
@@ -6,6 +6,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   // Calls generatePassword Function
   var password = generatePassword();
+  console.log(password);
   // Chooses where to put the password on website
   var passwordText = document.querySelector("#password");
   // Replaces the previous text with generated password
@@ -41,25 +42,20 @@ var lowercaseAlphabet = upperCaseAlphabet.map(element => {
 var specialCharacters = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',',',']','^','_','`','{','|','}','~',`\\`];
 var numbersArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-// Starts generatePassword Function
-function generatePassword() {
-  // Defining what passwordCharacters to add to later
-  var passwordCharacters = [];
-  // Defining what passwordCharacters to add to later
-  var password = [];
+function passwordOption() {
   // Stores the desiered password length in characterAmount
   var characterAmount = prompt('How many characters do you want your password to be?', 15);
   // To make sure data is clean
   if (characterAmount === null) {
     // If user cancels initial prompt
-    return 'Password Generator Exited';
+    alert('Password Generator Exited');
+    return;
     // Tells user password parameters and restarts function
   } else if (characterAmount < 8 || characterAmount > 128) {
     alert('Passwords must be between 8 and 128 characters');
-    characterAmount = 0;
-    generatePassword();
-
-    // Tells user to use numbers for selection
+    // characterAmount = 0;
+    return;
+  // Tells user to use numbers for selection
   // } else if(typeof characterAmount != 'number') {
   //   alert('You must use numbers for your selection');
   //   generatePassword();
@@ -67,46 +63,71 @@ function generatePassword() {
 
   // Include lowercase?
   var wantLowercase = confirm('Do you want to include lowercase characters?');
-  // Adds lowecase array to the passwordCharacters array
-  if (wantLowercase == true) {
-    passwordCharacters = passwordCharacters.concat(lowercaseAlphabet);
-  };
+ 
   // Include UpperCase?
   var wantUppercase = confirm('Do you want to include uppercase characters?');
-  // Adds uppercase array to the passwordCharacters array
-  if (wantUppercase == true) {
-    passwordCharacters = passwordCharacters.concat(upperCaseAlphabet);
-  };
+
   // Include Special Characters?
   var wantSpecial = confirm('Do you want to include special characters?');
-  // Adds uppercase array to the passwordCharacters array
-  if (wantSpecial == true) {
-    passwordCharacters = passwordCharacters.concat(specialCharacters);
-  };
+
   // Include Numbers?
   var wantNumber = confirm('Do you want to include numbers?');
-  // Adds uppercase array to the passwordCharacters array
-  if (wantNumber == true) {
-    passwordCharacters = passwordCharacters.concat(numbersArray);
-  };
+
+   // If user cancels all selections
+   if (wantLowercase == false && 
+    wantUppercase == false &&
+    wantSpecial == false &&
+    wantNumber == false) {
+      alert("Your password has to be made of something!\nLet's try this again... Shall we?");
+      return;
+    }  
   
-  // If user cancels all selections
-  if (wantLowercase == false && 
-      wantUppercase == false &&
-      wantSpecial == false &&
-      wantNumber == false) {
-        alert("Your password has to be made of something!\nLet's try this again... Shall we?")
-        // If didn't include any send back to the start
-        generatePassword();
-      }  
+  var obj = {
+    characterAmount: characterAmount,
+    wantLowercase: wantLowercase,
+    wantUppercase: wantUppercase,
+    wantSpecial: wantSpecial,
+    wantNumber: wantNumber
+  }
 
-  // Randomly selects numbers = characterAmount from passwordCharacters and adds them to the password array
-  for (let i = 0; i < characterAmount; i++) {
-    password.push(passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)])
+  return obj;
+}
+
+// Starts generatePassword Function
+function generatePassword() {
+
+    var options = passwordOption()
+    console.log(options);
+    // Defining what passwordCharacters to add to later
+    var passwordCharacters = [];
+    // Defining what passwordCharacters to add to later
+    var password = [];
+    //
+    if (options.wantLowercase == true) {
+      passwordCharacters = passwordCharacters.concat(lowercaseAlphabet);
+    };
+    if (options.wantUppercase == true) {
+      passwordCharacters = passwordCharacters.concat(upperCaseAlphabet);
+    };
+    if (options.wantSpecial == true) {
+      passwordCharacters = passwordCharacters.concat(specialCharacters);
+    };
+    if (options.wantNumber == true) {
+      passwordCharacters = passwordCharacters.concat(numbersArray);
+    };
+  
+    console.log(passwordCharacters);
+    console.log(options.characterAmount);
+  
+    // Randomly selects numbers = characterAmount from passwordCharacters and adds them to the password array
+    for (let i = 0; i < options.characterAmount; i++) {
+      password.push(passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)])
+    };
+  
+    // Changes password characters from array with characterAmount items to one string
+    password = password.join('');
+  
+    console.log(password);
+  
+    return password;
   };
-
-  // Changes password characters from array with characterAmount items to one string
-  password = password.join('');
-
-  return password;
-};
